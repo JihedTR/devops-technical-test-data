ab97f96d224c45cfea95514b0fcea027b49ac1c2
Jihed TRIFI <149218381+JihedTR@users.noreply.github.com>
2024-02-06 14:08:04 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,34 @@
-echo "from bash" 
+#!/bin/bash
+
+# Define repository and branch information
+REPO_URL="https://github.com/JihedTR/devops-technical-test-data.git"
+
+git clone --branch $BRANCH $REPO_URL
+cd devops-technical-test-data
+
+branch_name=$(date +'%d-%m-%Y')"-test"
+git checkout $branch_name
+
+base_directory="data"
+
+commit_count=$(git rev-list --count origin/$branch_name)
+commit_number=$(($commit_count - 18))
+
+echo $commit_number
+
+directory_name="${base_directory}/N+${commit_number}"
+mkdir -p "$directory_name"
+
+#commit_count=$(git rev-list --count origin/$BRANCH)
+
+#mkdir -p $backup_dir/N$commit_count
+
+MODIFIED_FILES=$(git diff-tree --name-only -r --no-commit-id  origin/$branch_name)
+
+cp $MODIFIED_FILES  $directory_name
+
+git config --local user.email "jihed.trifi@gmail.com"
+git config --local user.name "JihedTR"
+git add -A
+git commit -a -m "Updated "
+git push
